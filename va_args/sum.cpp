a2735ea5cb220b406d408e0b3eb6d910c6df6d71
ChunYunMuXue <1851738605@qq.com>
2023-10-04 10:32:37 +0800

    va_args

---
+++ va_args/sum.cpp
@@ -0,0 +1,16 @@
+//晦暗的宇宙，我们找不到光，看不见尽头，但我们永远都不会被黑色打倒。——Quinn葵因
+#include<bits/stdc++.h>
+#define ll long long
+
+ll sum(int n,...){
+    ll ans = 0;
+    va_list A;
+    va_start(A,n);
+    for(int i = 1;i <= n;++i)ans += va_arg(A,int);
+    va_end(A);
+    return ans;
+}
+
+int main(){
+    std::cout<<sum(3,1,2,3);
+}
---
+++ va_args/vprintf.cpp
@@ -0,0 +1,14 @@
+//晦暗的宇宙，我们找不到光，看不见尽头，但我们永远都不会被黑色打倒。——Quinn葵因
+#include<bits/stdc++.h>
+#define ll long long
+
+inline void printf(char *format,...){
+    va_list A;
+    va_start(A,format);
+    vprintf(format,A);
+    va_end(A);
+}
+
+int main(){
+    printf("%d %d %d",11,45,14);
+}
